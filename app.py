7e7110f440ab09867d8d8feed5cb21620c075d51
rttitity <jinwoo25803@gmail.com>
2025-05-23 16:07:03 +0900

    세션 CDB for Redis 활용

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flaskext.markdown import Markdown
 from pybo import db, migrate, scheduler  # ✅ 확장 객체 import
 from pybo.scheduler import fetch_and_store_positions    # 스케줄 라이브러리
+from flask_session import Session
 
 from datetime import datetime
 import socket
 import config
+import redis
 
 
 def create_app():
@@ -34,6 +36,16 @@ def create_app():
     # Markdown
     Markdown(app, extensions=['nl2br', 'fenced_code'])
 
+    # Redis 연결 구성
+    app.config['SESSION_REDIS'] = redis.StrictRedis(
+        host=app.config['SESSION_REDIS_HOST'],
+        port=app.config['SESSION_REDIS_PORT'],
+        # password=app.config['SESSION_REDIS_PASSWORD'],
+        decode_responses=True
+    )
+
+    Session(app)
+
     # ✅ APScheduler 초기화 및 작업 등록
     # scheduler.init_app(app)
     # scheduler.start()
@@ -44,19 +56,20 @@ def create_app():
     #     seconds=30
     # )
 
-    @app.before_request
-    def set_session_id():
-        from flask import request, session
-        session['_id'] = request.cookies.get('session', 'N/A')
+    # Flask 세션 사용시 쿠키에 세션id 저장 - 쿠키 헤더 초과 오류 발생
+    # @app.before_request
+    # def set_session_id():
+    #     from flask import request, session
+    #     session['_id'] = request.cookies.get('session', 'N/A')
 
 
     @app.context_processor
     def inject_server_info():
-        from flask import session
+        from flask import request
         return {
             'hostname': socket.gethostname(),
             'server_ip': socket.gethostbyname(socket.gethostname()),
-            'session_id': session.get('_id', 'N/A'),
+            'session_id': request.cookies.get('session', 'N/A'),
             'current_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         }
 
